0cafb8df3730e896161080ed0c4c34e629ecce03
Nathan Ringo <nathan@remexre.com>
2023-10-10 09:26:49 -0500

    Artifact done?

--- 2023_SLE_Nanopass_Attribute_Grammars/build.sh
+++ 2023_SLE_Nanopass_Attribute_Grammars/build.sh
@@ -6,3 +6,12 @@ test ! -d nanopass-ag || rm -rf nanopass-ag
 git clone git@github.umn.edu:ringo025/nanopass-ag.git
 cp example.nag nanopass-ag/compiler/src/example.nag
 docker build -t melt-umn/nanopass-ags .
+docker save melt-umn/nanopass-ags > 2023_SLE_Nanopass_Attribute_Grammars.docker
+files=(
+	2023_SLE_Nanopass_Attribute_Grammars.docker
+	Abstract.txt
+	Getting-Started.txt
+	paper.pdf
+)
+tar czf 2023_SLE_Nanopass_Attribute_Grammars.tar.gz "${files[@]}"
+zip 2023_SLE_Nanopass_Attribute_Grammars.zip "${files[@]}"
